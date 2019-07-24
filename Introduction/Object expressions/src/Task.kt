c5be651ac8f48ea3ce5e6a2d6473ee202b61e8f0
Rafael Tavares <rafaeltavares91@gmail.com>
2019-07-23 23:03:27 -0300

    anonymous inner class and lambdas exercises

--- Introduction/Object expressions/src/Task.kt
+++ Introduction/Object expressions/src/Task.kt
@@ -2,6 +2,8 @@ import java.util.*
 
 fun getList(): List<Int> {
     val arrayList = arrayListOf(1, 5, 2)
-    Collections.sort(arrayList, object {})
+    Collections.sort(arrayList, object: Comparator<Int> {
+        override fun compare(a: Int, b: Int) =  b - a
+    })
     return arrayList
 }
