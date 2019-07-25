ba810b515f0bcc28cb5025b4bcdafcc4ec7a8273
Rafael Tavares <murilommms@gmail.com>
2019-07-25 02:25:21 -0300

    Comparasion and In range exercises

--- Conventions/Comparison/src/Task.kt
+++ Conventions/Comparison/src/Task.kt
@@ -1,5 +1,9 @@
 data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
-    /* TODO */
+    override fun compareTo(b: MyDate) = when {
+        year != b.year -> year - b.year
+        month != b.month -> month - b.month
+        else -> dayOfMonth - b.dayOfMonth
+    }
 }
 
 fun compare(date1: MyDate, date2: MyDate) = date1 < date2
